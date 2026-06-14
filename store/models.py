43d8dbf1c32aade0faf7bd80b1540acea52dc542
karan <karanratate950@gmail.com>
2026-06-14 11:39:39 +0530

    store and carts funtionality

--- store/models.py
+++ store/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     slug             = models.SlugField(max_length=200, unique=True)
     description      = models.TextField(max_length=255, blank=True)
     price            = models.IntegerField()
-    image            = models.ImageField(upload_to='photos/products')
+    images           = models.ImageField(upload_to='photos/products')
     stock            = models.IntegerField()
     is_available     = models.BooleanField(default=True)
     category         = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -21,3 +21,29 @@ class Product(models.Model):
     
     def __str__(self):
         return self.product_name
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+    
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+class Variation(models.Model):
+    product             = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category  = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value     = models.CharField(max_length=100)
+    is_active           = models.BooleanField(default=True)
+    created_date        = models.DateTimeField(auto_now=True)
+    
+    objects = VariationManager()
+    
+    def __str__(self):
+        return self.variation_value
+
