c5670ff563298731414e3054396075a86c2f64e8
karan <karanratate950@gmail.com>
2026-07-14 17:33:38 +0530

    dashboard edit profile order details

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,19 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from .models import Account, UserProfile
+from django.utils.html import format_html
 
-# Register your models here. 
+# Register your models here.
 
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'username','last_login','date_joined','is_active')
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
-    readonly_field = ('last_login', 'date_joined')
+    readonly_fields = ('last_login', 'date_joined')
     ordering = ('-date_joined',)
-    
-    
+
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
-    
-admin.site.register(Account, AccountAdmin)
 
+class UserProfileAdmin(admin.ModelAdmin):
+
+    def thumbnail(self, obj):
+        if obj.profile_picture:
+            return format_html(
+                '<img src="{}" width="30" height="30" style="border-radius:50%;">',
+                obj.profile_picture.url
+            )
+        return "-"
+
+    thumbnail.short_description = "Profile Picture"
+
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- k_kart/settings.py
+++ k_kart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ic57q1a-lpz&lwg(yi^7z$7u=7e$m&9)3jhgu-pk4mn644g@a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -140,8 +141,8 @@ MESSAGE_TAGS = {
 }
 
 # SMTP configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'karanratate855@gmail.com'
-EMAIL_HOST_PASSWORD = 'fubi fgyk uxah zupu'
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
