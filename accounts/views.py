9026a7fc3dcbc909af7c3b2a6386ee82f6dab806
karan <karanratate950@gmail.com>
2026-07-14 23:52:01 +0530

    small changes

--- accounts/views.py
+++ accounts/views.py
@@ -35,7 +35,7 @@ def register(request):
             # Create a user profile
             profile = UserProfile()
             profile.user_id = user.id
-            profile.profile_picture = 'default/default-user.png'
+            profile.profile_picture = 'default/default-user.jpeg'
             profile.save()
 
             # USER ACTIVATION
